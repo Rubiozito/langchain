3708c83309dbd1bbc5fff94a8777ca4975a73886
Rubiozito <rafaelrubiocarnes@gmail.com>
2025-03-12 16:39:52 -0300

    feat(chat); add questions

--- chat_sofia.py
+++ chat_sofia.py
@@ -13,7 +13,7 @@ load_dotenv()
 model = ChatOpenAI(model="gpt-4o-mini", api_key=os.getenv("OPENAI_API_KEY"))
 
 mongo_uri = os.getenv("MONGO_URI")
-history = MongoDBChatMessageHistory(connection_string=mongo_uri, session_id="user_chat4", database_name="langchain-socium")
+history = MongoDBChatMessageHistory(connection_string=mongo_uri, session_id="user_chat5", database_name="langchain-socium")
 
 system_message = SystemMessage(
     content="Você é Sofia, uma atendente virtual da empresa Socium. "
@@ -21,6 +21,8 @@ system_message = SystemMessage(
             "Você pode explicar sobre a empresa ou agendar uma reunião. "
             "Pergunte ao usuário o que deseja e classifique a intenção."
 )
+
+#Get history
 
 if not history.messages:
     history.add_message(system_message)
@@ -32,6 +34,9 @@ def get_recent_chat_history(history, max_messages=10):
     )
     return formatted_history
 
+# Prompt templates
+
+# Prompt de identificação da intenção do usuário
 identification_template = ChatPromptTemplate.from_messages([
     ("system", "Você é Sofia, uma atendente virtual da empresa Socium. "
                "Identifique a intenção do usuário entre estas opções: "
@@ -40,12 +45,14 @@ identification_template = ChatPromptTemplate.from_messages([
     ("human", "{user_input}"),
 ])
 
+# Prompt de apresentação da empresa
 presentation_template = ChatPromptTemplate.from_messages([
     ("system", f"Você é Sofia, uma atendente virtual da empresa Socium. você deve apresentar a empresa com base nas seguintes informações: {infos_socium}."),
     ("human", "{user_input}"),
 ])
 
 
+# Prompt de agendamento de reunião
 schedule_template = ChatPromptTemplate.from_messages([
     ("system", "Você é Sofia, uma atendente virtual da empresa Socium. "
                "Ajude o usuário a agendar uma reunião. Pergunte sobre "
@@ -53,6 +60,22 @@ schedule_template = ChatPromptTemplate.from_messages([
     ("human", "{user_input}"),
 ])
 
+
+# Prompt de pergunas sobre o cliente
+client_questions_template = ChatPromptTemplate.from_messages([
+    ("system", '''Você é Sofia, uma atendente virtual da empresa Socium. 
+               Você deve fazer 3 perguntas para o cliente.
+            <perguntas>
+               1. Qual o numero aproximado de funcionários da empresa?.
+               2. Qual a idade da empresa?.
+               3. A empresa está em processo de transformação tecnológica?
+            </perguntas>
+     Faça uma pergunta por vez e aguarde a resposta do cliente.
+     '''),
+    ("human", "{user_input}"),
+])
+
+#chat loop
 while True:
     user_input = input("Você: ")
     if user_input.lower() == "sair":
@@ -68,7 +91,20 @@ while True:
     if "conhecer a empresa" in classification:
         chain = presentation_template | model | StrOutputParser() 
     elif "agendar uma reunião" in classification:
-        chain = schedule_template | model | StrOutputParser() 
+        print("\nSofia: Antes de prosseguirmos com o agendamento, preciso fazer algumas perguntas sobre sua empresa.")
+
+        for i in range(3):
+            response = client_questions_template | model | StrOutputParser()
+            question = response.invoke({"user_input": user_input, "chat_history": chat_history})
+            
+            print(f"Sofia: {question}")
+            user_answer = input("Você: ")
+            
+            history.add_message(HumanMessage(content=user_answer))  # Salva a resposta no histórico
+
+        print("\nSofia: Obrigada! Agora podemos continuar com o agendamento.")
+
+        chain = schedule_template | model | StrOutputParser()
     else:
         chain = RunnableLambda(lambda x: "Desculpe, não entendi sua solicitação. Você deseja conhecer a empresa ou agendar uma reunião?")
 
